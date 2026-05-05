56db453172ac969c141ca49a62beb59600edad25
Mauro Mura <mauro.mura@inginfo.eu>
2026-05-05 22:31:16 +0200

    refactor(core): replace `MessageService` reference with `FilterableSubscriber` in `MessageFilter`
    - Updated `@see` references and imports to align with refactored messaging architecture.

--- jentic-core/src/main/java/dev/jentic/core/filter/MessageFilter.java
+++ jentic-core/src/main/java/dev/jentic/core/filter/MessageFilter.java
@@ -1,7 +1,7 @@
 package dev.jentic.core.filter;
 
 import dev.jentic.core.Message;
-import dev.jentic.core.MessageService;
+import dev.jentic.core.messaging.FilterableSubscriber;
 
 import java.util.function.Predicate;
 
@@ -45,82 +45,82 @@ import java.util.function.Predicate;
  * </ul>
  *
  * @since 0.2.0
- * @see MessageService
+ * @see FilterableSubscriber
  * @see MessageFilterBuilder
  * @see Message
  */
 @FunctionalInterface
 public interface MessageFilter extends Predicate<Message> {
-    
+
     /**
      * Test if the message matches this filter
-     * 
+     *
      * @param message the message to test
      * @return true if the message matches the filter criteria
      */
     @Override
     boolean test(Message message);
-    
+
     /**
      * Create a filter that matches all messages
-     * 
+     *
      * @return filter that always returns true
      */
     static MessageFilter acceptAll() {
         return message -> true;
     }
-    
+
     /**
      * Create a filter that rejects all messages
-     * 
+     *
      * @return filter that always returns false
      */
     static MessageFilter rejectAll() {
         return message -> false;
     }
-    
+
     /**
      * Create a filter from a predicate
-     * 
+     *
      * @param predicate the predicate to use
      * @return message filter wrapping the predicate
      */
     static MessageFilter of(Predicate<Message> predicate) {
         return predicate::test;
     }
-    
+
     /**
      * Combine this filter with another using AND logic
-     * 
+     *
      * @param other the other filter
      * @return combined filter (this AND other)
      */
     default MessageFilter and(MessageFilter other) {
         return message -> this.test(message) && other.test(message);
     }
-    
+
     /**
      * Combine this filter with another using OR logic
-     * 
+     *
      * @param other the other filter
      * @return combined filter (this OR other)
      */
     default MessageFilter or(MessageFilter other) {
         return message -> this.test(message) || other.test(message);
     }
-    
+
     /**
      * Negate this filter
-     * 
+     *
      * @return negated filter (NOT this)
      */
     default MessageFilter negate() {
         return message -> !this.test(message);
     }
-    
+
     /**
      * Create a filter builder for fluent API
-     * 
+     *
      * @return new message filter builder
      */
     static MessageFilterBuilder builder() {
